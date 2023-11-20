a1148a0924638fcb6fc3c2eff205394474f140bf
Delorme07 <yvesconanan07@gmail.com>
2023-11-20 21:34:55 +0800

    added notification tab for admin and adjust its style6.css, dashboard parent's end, adjust its style3.css, added img

--- Parent/Dashboard-PARENT.php
+++ Parent/Dashboard-PARENT.php
@@ -1,6 +1,7 @@
 
 <?php include('../templates/Header.php'); ?>
 <link rel="stylesheet" href="./css/style3.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
 <body>
 <div class="container1">
         <div class="column1">
@@ -22,6 +23,52 @@
                
             </div>
             </div>
+
+            <div class="Notif-table">
+            <div class="NotifTable_section">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>NOTIFICATIONS</th>
+                            <th>ACTION</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><img src="./images/notif1.png" alt="">
+                                <span class="notification-text">
+                                    <strong>Why vaccine is important to baby?</strong> <br>Deciding to vaccinate your child: Common concerns
+                                </span>
+                            </td>
+                            <td>
+                            <a href="#"><i class="fas fa-eye"></i></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><img src="./images/notif2.png" alt="">
+                                <span class="notification-text">
+                                    <strong>Needed vaccine from 0yrs old to 24 months</strong> <br>List of vaccine required by the government
+                                </span>
+                            </td>
+                            <td>
+                            <a href="#"><i class="fas fa-eye"></i></a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><img src="./images/notif3.png" alt="">
+                                <span class="notification-text">
+                                    <strong>Benefits of vaccines for future generations</strong> <br>Important reasons to vaccinate your child
+                                </span>
+                            </td>
+                            <td>
+                            <a href="#"><i class="fas fa-eye"></i></a>
+                            </td>
+                        </tr>
+                    
+                    </tbody>
+                </table>
+            </div>
+        </div>
         </div>
 </div>
     <script src="./js/index.js"></script>
